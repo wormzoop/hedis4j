392dbf12105883113771c09d3da545a00591d959
wormzoop <446706489@qq.com>
2018-02-03 17:28:43 +0800

    hedisclient

---
+++ src/com/zoop/HedisClient.java
@@ -0,0 +1,27 @@
+package com.zoop;
+
+/**
+ * hedis客户端
+ */
+public class HedisClient {
+
+	/**
+	 * 存数据
+	 * @param key
+	 * @param value
+	 * @param time 毫秒
+	 */
+	public void set(String key, Object value, int time) {
+		
+	}
+	
+	/**
+	 * 取数据
+	 * @param key
+	 * @return
+	 */
+	public Object get(String key) {
+		return null;
+	}
+	
+}
